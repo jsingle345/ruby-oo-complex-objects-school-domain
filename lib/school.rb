dd45ed76599d62cf903a2fd291d2d44c0989396d
jsingle345 <jsingle345@gmail.com>
2020-09-16 21:56:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School
+    attr_reader :school_name, :roster, :student_name
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+
+    def add_student(student_name, grade)
+        if @roster[grade]
+        @roster[grade] << student_name
+        else
+        @roster[grade] = []
+        @roster[grade] << student_name
+        end
+    end 
+
+    def grade(level)
+        @roster.detect do|grade, students|
+            if grade == level
+            return students
+            end
+        end
+    end
+
+    def sort
+        sort_hash = {}
+
+        @roster.each do |grade, students|
+            sort_hash[grade] =  students.sort 
+        end
+        sort_hash
+    end
+end
